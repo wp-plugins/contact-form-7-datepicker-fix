ab8a9cf02cb08ebab782fa32bf13e7aed28494b2
Hackbard23 <Hackbard23@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-13 12:06:44 +0000

    New Version
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7-datepicker-fix/trunk@771613 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cf7-datepicker-ie-fixer.php
+++ cf7-datepicker-ie-fixer.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Contact Form 7 IE DatePicker and Number Spinner Fix
 Description: Add the DatePicker and Number Spinner functionality to the Internet Explorer in Contact Form 7
-Version: 2.1
+Version: 2.2
 Author: Benjamin Klein
 Author URI: http://touchdata.net
 License: GPL2
